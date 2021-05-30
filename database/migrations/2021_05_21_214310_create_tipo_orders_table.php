ba9f0686f6f95f946e4c9bd3892463244867d63b
chocho <jorgeantonio16@gmail.com>
2021-05-30 10:04:15 -0500

    creacion table orders

--- database/migrations/2021_05_21_214310_create_tipo_orders_table.php
+++ database/migrations/2021_05_21_214310_create_tipo_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTipoOrdensTable extends Migration
+class CreateTipoOrdersTable extends Migration
 {
     /**
      * Run the migrations.
