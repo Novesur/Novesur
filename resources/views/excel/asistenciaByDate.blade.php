cfc1dd6c1520e68647d62c93d189c2aa72425e43
Jorge Sarmiento <jorgeantonio16@gmail.com>
2023-09-13 22:13:39 -0500

    13092023

--- resources/views/excel/asistenciaByDate.blade.php
+++ resources/views/excel/asistenciaByDate.blade.php
@@ -12,6 +12,7 @@
         <tr>
             <th><b>Fecha</b></th>
             <th><b>Hora</b></th>
+            <th><b>Minutos Tardanza</b></th>
             <th><b>Personal</b></th>
             <th><b>DNI</b></th>
             <th><b>Zona</b></th>
@@ -28,6 +29,18 @@
              
                 <td>{{$data->fecha}}</td>
                 <td>{{$data->tiempo}}</td>
+                @php
+
+                $apertura = new DateTime('08:00:00');
+                $cierre = new DateTime($data->tiempo);
+                $tiempo = $apertura->diff($cierre);
+                @endphp
+
+                @if($apertura <= $cierre)
+                <td>{{$tiempo->format('%i')}}</td>
+                @else
+                <td>0</td>
+                @endif
                 <td>{{$data->personal}}</td>
                 <td>{{$data->DNI}}</td>
                 <td>{{$data->zonal}}</td>
